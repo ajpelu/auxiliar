7f59c8365e9946cab6afd88f30666cdee8f203a8
Antonio <ajpelu@gmail.com>
2017-09-05 10:34:32 +0200

    fix typo error

--- R/shapirosNormal.R
+++ R/shapirosNormal.R
@@ -17,6 +17,7 @@
 #' \item \code{statistic} is the value of the Shapiro-Wilk statistic
 #' \item \code{p_value} an approximate p-value for the test. See
 #' \code{\link[stats]{shapiro.test}}
+#' }
 
 shapirosNormal <- function(df, resp_var, factor_vars) {
   rv <- enquo(resp_var)
